b94b1a00bb2d467d3646e74734cc5c808f15cc53
Zachary Elliott <contact@zell.io>
2020-02-06 00:56:20 -0500

    Moves default values to config values

--- src/pdf2laser.h
+++ src/pdf2laser.h
@@ -10,65 +10,6 @@ extern "C" {
 }
 #endif
 
-/* #define VECTOR_PASSES (3) */
-
-/** Default on whether or not auto-focus is enabled. */
-#define AUTO_FOCUS (true)
-
-/** Default bed height (y-axis) in pts. */
-#define BED_HEIGHT (864)
-
-/** Default bed width (x-axis) in pts. */
-#define BED_WIDTH (1728)
-
-/** Default for debug mode. */
-#define DEBUG (true)
-
-/** Number of characters allowable for a filename. */
-#define FILENAME_NCHARS (1024)
-
-/** default on whether or not the result is supposed to be flipped along the X
- * axis.
- */
-#define FLIP (false)
-
-/** Default mode for processing raster engraving (varying power depending upon
- * image characteristics).
- */
-#define RASTER_MODE_DEFAULT (RASTER_MODE_MONO)
-
-/** Default power level for raster engraving */
-#define RASTER_POWER_DEFAULT (40)
-
-/** Whether or not the raster printing is to be repeated. */
-#define RASTER_REPEAT (1)
-
-/** Default speed for raster engraving */
-#define RASTER_SPEED_DEFAULT (100)
-
-/** Default resolution is 600 DPI */
-#define RESOLUTION_DEFAULT (600)
-
-/** Pixel size of screen (0 is threshold).
- * FIXME - add more details
- */
-#define SCREEN_DEFAULT (8)
-
-/** Temporary directory to store files. */
-#define TMP_DIRECTORY "/tmp"
-
-/** FIXME */
-#define VECTOR_FREQUENCY_DEFAULT (5000)
-
-/* /\** Default power level for vector cutting. *\/ */
-/* #define VECTOR_POWER_DEFAULT (50) */
-
-/* /\** Default speed level for vector cutting. *\/ */
-/* #define VECTOR_SPEED_DEFAULT (30) */
-
-/* static void range_checks(print_job_t *print_job); */
-/* static void usage(int rc, const char* const msg); */
-
 int main(int argc, char *argv[]);
 
 #ifdef __cplusplus
---
+++ src/pdf2laser_type.c
@@ -0,0 +1,100 @@
+#include "pdf2laser_type.h"
+#include "type_vector_list.h"
+
+vector_list_config_t *print_job_append_vector_list(print_job_t *self, vector_list_t *vector_list, int32_t red, int32_t green, int32_t blue)
+{
+	vector_list_config_t *current_config = self->configs;
+	vector_list_config_t *new_config = vector_list_config_create();
+
+	new_config->id = vector_list_config_rgb_to_id(red, green, blue);
+	new_config->vector_list = vector_list;
+
+	if (current_config == NULL) {
+		new_config->index = 0;
+		self->configs = new_config;
+		return new_config;
+	}
+
+	int32_t index = 1;
+	while (current_config->next != NULL) {
+		index += 1;
+		current_config = current_config->next;
+	}
+	new_config->index = index;
+	current_config->next = new_config;
+
+	return new_config;
+}
+
+vector_list_config_t *print_job_append_new_vector_list(print_job_t *self, int32_t red, int32_t green, int32_t blue)
+{
+	vector_list_t *vector_list = vector_list_create();
+	return print_job_append_vector_list(self, vector_list, red, green, blue);
+}
+
+vector_list_config_t *print_job_clone_last_vector_list(print_job_t *self, int32_t red, int32_t green, int32_t blue)
+{
+	vector_list_config_t *config = self->configs;
+	while (config->next) {
+		config = config->next;
+	}
+	vector_list_t *vector_list = vector_list_shallow_clone(config->vector_list);
+	return print_job_append_vector_list(self, vector_list, red, green, blue);
+}
+
+vector_list_config_t *print_job_find_vector_list_config_by_id(print_job_t *self, uint32_t id)
+{
+	for (vector_list_config_t *config = self->configs;
+		 config != NULL;
+		 config = config->next) {
+
+		if (config->id == id)
+			return config;
+	}
+	return NULL;
+}
+
+vector_list_config_t *print_job_find_vector_list_config_by_rgb(print_job_t *self, int32_t red, int32_t green, int32_t blue)
+{
+	return print_job_find_vector_list_config_by_id(self, vector_list_config_rgb_to_id(red, green, blue));
+}
+
+char *print_job_inspect(print_job_t *self)
+{
+	char *s = calloc(30, sizeof(char));
+	snprintf(s, 30, "<PrintJob:0x%016lx>", (uintptr_t)self);
+	return s;
+}
+
+char *print_job_to_string(print_job_t *self)
+{
+	int32_t length = 0;
+	vector_list_config_t *configs = self->configs;
+	while (configs) {
+		length += 1;
+		configs = configs->next;
+	}
+
+	int32_t max_str_size =
+		6 + strnlen(self->name, 1024) + 39 + (65 * (int)length) + 1;
+	char *s = calloc(max_str_size, sizeof(char));
+
+	snprintf(s, max_str_size,
+			 "Job: %s\nRaster: speed=%d power=%d dpi=%d\n",
+			 self->name,
+			 self->raster->speed,
+			 self->raster->power,
+			 self->raster->resolution);
+
+	for (vector_list_config_t *config = self->configs;
+		 config != NULL;
+		 config = config->next) {
+
+		strncat(s, vector_list_config_to_string(config), max_str_size);
+
+		if (config->next)
+			strncat(s, "\n", max_str_size);
+	}
+
+	return s;
+}
